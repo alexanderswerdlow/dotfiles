32c080401b4ede24073934cf77c8082183e64cec
Alexander Swerdlow <aswerdlow1@gmail.com>
2021-02-15 10:12:50 -0800

    Switched to starship

--- aliases.zsh
+++ aliases.zsh
@@ -11,10 +11,12 @@ alias reload="exec zsh"
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
+alias '..'="cd .."
+alias '...'="cd ../../"
+alias gp="git peek"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-alias watch="npm run watch"
 
 # Vagrant
 alias v="vagrant global-status"
@@ -30,7 +32,6 @@ alias docker-composer="docker-compose"
 # Git
 alias gst="git status"
 alias gb="git branch"
-alias gc="git checkout"
 alias gl="git log --oneline --decorate --color"
 alias amend="git add . && git commit --amend --no-edit"
 alias commit="git add . && git commit -m"
@@ -50,8 +51,32 @@ alias venv="python3 -m venv"
 alias act="source ./bin/activate"
 alias deact="deactivate"
 
-alias seas="ssh swerdlow@lnxsrv09.seas.ucla.edu"
-alias edit="subl ~/.zshrc"
+alias system_pip="/usr/bin/pip3"
+alias system_python="/usr/bin/python3"
+alias upgrade="pip install --upgrade pip"
+
+# Brew
+
+alias brewd="brew doctor"
+alias brewi="brew install"
+alias brewr="brew uninstall"
+alias brews="brew search"
+alias brewu="brew update && brew upgrade && brew cleanup"
+
+
+alias empty-trash="sudo rm -frv /Volumes/*/.Trashes; \
+                   sudo rm -frv ~/.Trash; \
+                   sudo rm -frv /private/var/log/asl/*.asl; \
+                   sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+
+alias local-ip="ipconfig getifaddr en0"
+
+alias seas="ssh -R 52698:localhost:52698 swerdlow@lnxsrv09.seas.ucla.edu"
+alias seas6="ssh -R 52698:localhost:52698 swerdlow@lnxsrv06.seas.ucla.edu"
+alias seas11="ssh -R 52698:localhost:52698 swerdlow@lnxsrv11.seas.ucla.edu"
+alias edit="code ~/.dotfiles"
+alias aliases="subl ~/.dotfiles/aliases.zsh"
+alias paths="subl ~/.dotfiles/path.zsh"
 alias untar="tar -xvzf"
 
 alias torguard="sudo wg-quick up wg0 >/dev/null 2>&1"
@@ -59,6 +84,60 @@ alias algo="sudo wg-quick up wg1 >/dev/null 2>&1"
 alias off="sudo wg-quick down wg0 >/dev/null 2>&1; sudo wg-quick down wg1 >/dev/null 2>&1"
 alias wireguard="sudo wg"
 alias run_rsync='rsync -azP --exclude ".*/" --exclude ".*"'
+
+alias st='subl'
+alias o='a -e open' # quick opening files with xdg-open
+alias awake='caffeinate -d -i -s -u'
+
+alias startsc="sudo launchctl load -w /Library/LaunchAgents/Safe.Connect.client.plist; open -a '/Applications/SafeConnect.app/Contents/MacOS/scClient'"
+alias quitsc="osascript -e 'tell application \"SafeConnect.app\" to quit';sudo launchctl unload -w /Library/LaunchAgents/Safe.Connect.client.plist"
+alias home="cd ~/"
+
+cpu_str=$(sysctl -a | grep 'machdep.cpu.brand_string')
+arm64_cpu="Apple M1"
+
+if [[ "$cpu_str" == *"$arm64_cpu"* ]]; then
+    # Brew
+    alias armbrew="arch -arm64 /opt/homebrew/bin/brew"
+    alias abrew=armbrew
+    alias intelbrew="arch -x86_64 /usr/local/bin/brew"
+    alias ibrew=intelbrew
+    alias brew=abrew
+
+    # Python
+    alias intel_python3="/usr/local/bin/python3"
+    alias intel_python="ipython3"
+    alias intel_pip3="/usr/local/bin/pip3"
+    alias intel_pip="ipip3"
+
+    alias arm_python3="/opt/homebrew/bin/python3"
+    alias armpython="apython3"
+    alias armpip3="/opt/homebrew/bin/pip3"
+    alias armpip="apip3"
+
+    alias intel='arch -x86_64'
+
+    alias mvenv='mkvirtualenv'
+    alias rvenv='rmvirtualenv'
+    alias venv='workon'
+    alias act='workon'
+
+    alias code="code-insiders"
+else
+    # Python
+    alias brew_python="/usr/local/bin/python3"
+    alias brew_pip="/usr/local/bin/python3 -m pip"
+    alias system_python="/usr/bin/python3"
+    alias system_pip="/usr/bin/python3 -m pip"
+
+    export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+    export JAVA_15_HOME=$(/usr/libexec/java_home -v15)
+
+    alias java8='export JAVA_HOME=$JAVA_8_HOME && java -version'
+    alias java15='export JAVA_HOME=$JAVA_15_HOME && java -version'
+    alias cat='bat'
+    alias ls='exa'
+fi
 
 function abspath() {
     # generate absolute path from relative path
@@ -79,8 +158,53 @@ function abspath() {
 }
 
 function ucla() {
-	remote="/u/cs/ugrad/swerdlow/$(abspath $1 | sed 's/^.*aswerdlow\///')/"
-	local="$(abspath $1)/"
-	server="swerdlow@lnxsrv09.seas.ucla.edu"
-	run_rsync "$local" "$server:$remote"; fswatch -o . | while read f; do run_rsync "$local" "$server:$remote"; done
+    remote="/u/cs/ugrad/swerdlow/$(abspath $1 | sed 's/^.*Documents\///')/"
+    local="$(abspath $1)/"
+    server="swerdlow@lnxsrv09.seas.ucla.edu"
+    echo $remote
+    echo $local
+    run_rsync "$local" "$server:$remote"; fswatch -o . | while read f; do run_rsync "$local" "$server:$remote"; done
+}
+
+
+rga-fzf() {
+    RG_PREFIX="rga --files-with-matches"
+    local file
+    file="$(
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+            fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+                --phony -q "$1" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window="70%:wrap"
+    )" &&
+    echo "opening $file" &&
+    open "$file"
+}
+
+function sman() {
+    man $1 > /tmp/man && subl /tmp/man
+}
+
+
+function pman() {
+    man -t $1 | open -f -a PDF\ Expert
+}
+
+function hman() {
+    MANWIDTH=80 MANPAGER='col -bx' man $@ | open -b com.sublimetext.3
+}
+
+function jdk() {
+    version=$1
+    export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+    java -version
+}
+
+function timezsh() {
+  shell=${1-$SHELL}
+  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
+}
+
+function gcd() {
+  git clone "$1" && cd "$(basename "$1" .git)"
 }
--- path.zsh
+++ path.zsh
@@ -4,7 +4,14 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
-# Make sure coreutils are loaded before system commands
-# I've disabled this for now because I only use "ls" which is
-# referenced in my aliases.zsh file directly.
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export PATH="$HOME/Library/Python/3.8/bin:$PATH" # System Python
+export PATH="$HOME/Library/Python/3.9/bin:$PATH"
+export PATH="$HOME/Library/Python/3.9/lib/python/site-packages:$PATH" # Brew Python3 User Packages
+export PATH="$/usr/local/lib/python3.9/site-packages:$PATH" # Brew Python3 Packages
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+export PATH="$PATH:/Users/aswerdlow/.local/bin"
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="/usr/local/Homebrew/bin:$PATH"
