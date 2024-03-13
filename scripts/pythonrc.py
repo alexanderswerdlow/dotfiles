508b57b5ee94c1052424bb80624f6665668ecab7
Alexander Swerdlow <aswerdlow1@gmail.com>
2024-03-13 12:52:33 -0400

    update

--- scripts/pythonrc.py
+++ scripts/pythonrc.py
@@ -1,7 +1,9 @@
-from importlib import import_module
+from pathlib import Path
+from time import time
 
 def import_if_exists(module_name):
     try:
+        from importlib import import_module
         return import_module(module_name)
     except ModuleNotFoundError:
         return None
@@ -10,16 +12,4 @@ np = import_if_exists("numpy")
 torch = import_if_exists("torch")
 einx = import_if_exists("einx")
 library_ops = import_if_exists("image_utils.library_ops")
-Im = import_if_exists("image_utils.Im")
-
-# try:
-#     import sys
-#     import IPython
-#     sys.breakpointhook = IPython.embed
-# except ImportError as e:
-#     pass
-
-from pathlib import Path
-from time import time
-
-print("Initialized w/pythonrc")
+Im = getattr(import_if_exists("image_utils"), "Im")
